68d511619700892b63e7fdccc5c8a54745acdca3
Pedro Molina <pealmo98@gmail.com>
2023-12-09 23:04:58 -0500

    Implementación OpenApi usando Swagger

---
+++ src/main/java/com/example/terceranotaweb/configuration/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.example.terceranotaweb.configuration;
+
+import com.example.terceranotaweb.entities.Usuario;
+import com.example.terceranotaweb.services.UsuarioService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+
+    private final UsuarioService usuarioService;
+
+    public SecurityConfig(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, NoOpPasswordEncoder noOpPasswordEncoder)
+            throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(usuarioService).passwordEncoder(noOpPasswordEncoder);
+        return authenticationManagerBuilder.build();
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/rest/auth/**").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+}
--- src/main/java/com/example/terceranotaweb/configuration/SwaggerConfig.java
+++ src/main/java/com/example/terceranotaweb/configuration/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.terceranotaweb;
+package com.example.terceranotaweb.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
