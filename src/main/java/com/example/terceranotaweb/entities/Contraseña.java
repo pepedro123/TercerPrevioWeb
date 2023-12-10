2c2acdae4875be399c86a3b375f0f1a4dae8fcf1
Pedro Molina <pealmo98@gmail.com>
2023-12-09 22:16:15 -0500

    Creación Entidades

---
+++ src/main/java/com/example/terceranotaweb/entities/Contraseña.java
@@ -0,0 +1,18 @@
+package com.example.terceranotaweb.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+
+@Entity
+@Data
+public class Contraseña {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String contraseña;
+}
---
+++ src/main/java/com/example/terceranotaweb/entities/Usuario.java
@@ -0,0 +1,21 @@
+package com.example.terceranotaweb.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String telefono;
+
+}
